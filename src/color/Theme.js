a318e37878407d41cdc102d6b637de10b5ad30a0
Vactor0911 <vactor0911@gmail.com>
2024-07-08 22:12:02 +0900

    프로젝트 생성 이전으로 롤백

--- src/color/Theme.js
+++
@@ -1,5 +0,0 @@
-export const GRAY_COLOR = `#999`;
-export const WHITE_COLOR = `#fff`;
-export const BLACK_COLOR = "#000";
-export const RIGHT_PINK_COLOR = "#ffc8c8";
-export const DARK_PINK_COLOR = "#ea6060";
